c36f3f5c5598e88e842a5c72194f268dfd1bc5c1
Karim Yazbeck <karim.yazbek@st.ul.edu.lb>
2023-03-10 09:10:30 +0200

    first commit

---
+++ OpenAI GPT/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI_GPT.Models;
+using System.Diagnostics;
+
+namespace OpenAI_GPT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string mode = "normal")
+        {
+            ViewBag.Mode = mode;
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
